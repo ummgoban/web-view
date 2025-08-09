b9084386f645cfee81b7ca87bd58e7b95e24b032
99mini <itman390@gmail.com>
2025-08-09 21:39:48 +0900

    feat: bag icon

--- apps/client/src/domain/detail/page/detail.page.tsx
+++ apps/client/src/domain/detail/page/detail.page.tsx
@@ -2,15 +2,35 @@ import { useParams } from "react-router";
 
 import { DefaultLayout } from "@/component";
 import { useMarket } from "@/api/markets";
+import { LoadingCircle } from "@packages/ui";
 
 export const DetailPage = () => {
   const { id } = useParams();
 
-  const { data } = useMarket(Number(id));
+  const { data, isLoading } = useMarket(Number(id));
 
-  if (!data || !id) {
-    return <DefaultLayout appBarOptions={{ title: "DetailPage" }}>DetailPage {id}</DefaultLayout>;
+  if (isLoading) {
+    return <LoadingCircle animation />;
   }
 
-  return <DefaultLayout appBarOptions={{ title: data.name }}>{data.name}</DefaultLayout>;
+  // TODO:
+  // not found market page
+  if (!data) {
+    return <div>Not Found Market Page</div>;
+  }
+
+  return (
+    <DefaultLayout
+      appBarOptions={{
+        title: data.name,
+        RightContent: (
+          <a href="#" onClick={() => alert("TODO: 장바구니로 이동")}>
+            <img src="/icons/bag-bold.svg" alt="bag-bold" width={24} height={24} loading="lazy" />
+          </a>
+        ),
+      }}
+    >
+      {data.name}
+    </DefaultLayout>
+  );
 };
---
+++ apps/client/svg.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+declare module "*.svg" {
+  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default value;
+}
