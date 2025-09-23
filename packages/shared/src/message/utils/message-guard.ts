0b7b5a2c24d31df97c523b808e565e49a114df12
99mini <0mini9939@gmail.com>
2025-09-23 22:47:34 +0900

    style: insert enter

--- packages/shared/src/message/utils/message-guard.ts
+++ packages/shared/src/message/utils/message-guard.ts
@@ -7,14 +7,18 @@ type IsAppToWebOf<T extends AppToWebMethodType> = {
 export const isSafeAreaInsets: IsAppToWebOf<"SAFE_AREA_INSETS"> = (
   msg,
 ): msg is AppToWebPayload<"SAFE_AREA_INSETS"> => msg.type === "SAFE_AREA_INSETS";
+
 export const isInit: IsAppToWebOf<"INIT"> = (msg): msg is AppToWebPayload<"INIT"> =>
   msg.type === "INIT";
+
 export const isWebNavigation: IsAppToWebOf<"WEB_NAVIGATION"> = (
   msg,
 ): msg is AppToWebPayload<"WEB_NAVIGATION"> => msg.type === "WEB_NAVIGATION";
+
 export const isNativeHistory: IsAppToWebOf<"NATIVE_HISTORY"> = (
   msg,
 ): msg is AppToWebPayload<"NATIVE_HISTORY"> => msg.type === "NATIVE_HISTORY";
+
 export const isAuthorization: IsAppToWebOf<"AUTHORIZATION"> = (
   msg,
 ): msg is AppToWebPayload<"AUTHORIZATION"> => msg.type === "AUTHORIZATION";
